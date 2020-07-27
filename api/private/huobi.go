4b1aee3d843675bb91a7a03f214a831318fd0680
Xu Yang <xuyangcn@gmail.com>
2020-07-28 00:20:11 +0800

    Update huobi.go

--- api/private/huobi.go
+++ api/private/huobi.go
@@ -483,9 +483,9 @@ func (h *HuobiApi) ActiveOrders() ([]*models.Order, error) {
 func (h *HuobiApi) Order(trading string, settlement string, ordertype models.OrderType, price float64, amount float64) (string, error) {
 	//accountId, err := h.getAccountId()
 	accountId := "566720"
-	if err != nil {
-		return "", err
-	}
+	//if err != nil {
+	//	return "", err
+	//}
 	params := &url.Values{}
 	if ordertype == models.Ask {
 		params.Set("type", "buy-limit")
